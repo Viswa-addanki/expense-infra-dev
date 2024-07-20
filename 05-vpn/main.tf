2b3741f9f62d860dd3312ab1379b2af512ebf928
Viswa_addanki <a.viswanath007@gmail.com>
2024-07-20 11:43:22 +0530

    some-msg

---
+++ 05-vpn/main.tf
@@ -0,0 +1,28 @@
+resource "aws_key_pair" "vpn" {
+  key_name   = "openvpn"
+
+  #you can paste public key directly
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAmuYKI+sslc7RwdLglwsO8hhPTPAMTh60EpwowkFBp viswa@Viswanath"
+  public_key = file("~/.ssh/openvpn.pub")
+}
+
+
+module "vpn" {
+
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  key_name = aws_key_pair.vpn.key_name
+  name = "${var.project_name}-${var.environment}-vpn"
+
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+  # convert StringList to list and get first element
+  subnet_id = local.public_subnet_id
+  ami = data.aws_ami.ami_info.id
+ 
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-vpn"
+    }
+  )
+}
